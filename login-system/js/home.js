d612f308d204c9f8c9d4a9920328554b02c3de50
Sharadhi Hegde <73776648+sharadhih@users.noreply.github.com>
2024-06-19 08:26:54 +0530

    Delete login-system/js/home.js

--- login-system/js/home.js
+++
@@ -1,9 +0,0 @@
-let logOutBtn = document.getElementById("logOutBtn");
-let welcomeUser = document.getElementById("userName");
-let userName = localStorage.getItem("userName");
-
-welcomeUser.innerHTML = "Welcome " + userName;
-
-logOutBtn.addEventListener("click", function () {
-  window.location.href = "index.html";
-});
